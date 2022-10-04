d1ec9decb91948c8f8abf0360bdbd24c614aafcd
adriansoain <adriana.ansoain@540deg.com>
2022-10-04 17:27:59 +0200

    one-to-many-3

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,3 +1,25 @@
-export var fizzBuzz = function() {
-  return undefined
+export var fizzBuzz = function(numbers) {
+  return numbers.map(function(n) {
+    return fizzBuzzForNumber(n)
+  })
 }
+
+var fizzBuzzForNumber = function(aNumber) {
+  if (isDivisibleBy3(aNumber) && isDivisibleBy5(aNumber)) {
+    return 'FizzBuzz'
+  }
+  if (isDivisibleBy3(aNumber)) {
+    return 'Fizz'
+  }
+  if (isDivisibleBy5(aNumber)) {
+    return 'Buzz'
+  }
+  return aNumber.toString()
+}
+var isDivisibleBy = function(divisor) {
+  return function(n) {
+    return n % divisor === 0
+  }
+}
+var isDivisibleBy3 = isDivisibleBy(3)
+var isDivisibleBy5 = isDivisibleBy(5)
--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,7 +1,40 @@
 var myFizzBuzz = require('../src/fizzbuzz')
 
 describe('FizzBuzz', function() {
-  it('example test', function() {
-    expect(myFizzBuzz.fizzBuzz()).toEqual(undefined)
+  it('returns "1" for 1 number', function() {
+    expect(runFizzBuzz(1)).toEqual('1')
+  })
+  it('returns "2" for 2 number', function() {
+    expect(runFizzBuzz(2)).toEqual('2')
+  })
+  it('returns "Fizz" for 3 number', function() {
+    expect(runFizzBuzz(3)).toEqual('Fizz')
+  })
+  it('returns "Fizz" for 6 number', function() {
+    expect(runFizzBuzz(6)).toEqual('Fizz')
+  })
+  it('returns "Buzz" for 5 number', function() {
+    expect(runFizzBuzz(5)).toEqual('Buzz')
+  })
+  it('returns "Buzz" for 10 number', function() {
+    expect(runFizzBuzz(10)).toEqual('Buzz')
+  })
+  it('returns "Buzz" for 5 number', function() {
+    expect(runFizzBuzz(5)).toEqual('Buzz')
+  })
+  it('returns "FizzBuzz" for 15 number', function() {
+    expect(runFizzBuzz(15)).toEqual('FizzBuzz')
+  })
+  it('returns multiple numbers', function() {
+    expect((0, myFizzBuzz.fizzBuzz)([1, 2, 3, 4, 5])).toEqual([
+      '1',
+      '2',
+      'Fizz',
+      '4',
+      'Buzz'
+    ])
   })
 })
+var runFizzBuzz = function(n) {
+  return (0, myFizzBuzz.fizzBuzz)([n])[0]
+}
