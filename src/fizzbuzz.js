47e5dd8b09c804b0311b19614d822333cd72cf4b
adriansoain <adriana.ansoain@540deg.com>
2022-10-04 17:47:02 +0200

    one-to-many-2

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,42 +1,42 @@
-var predicate_1 = require('./predicate')
+export var myPredicate = require('./predicate')
 
 export var createFizzBuzz = function() {
   var ruleSet = [
     {
-      predicate: (0, predicate_1.and)(
-        (0, predicate_1.isDivisibleBy)(3),
-        (0, predicate_1.isDivisibleBy)(5)
+      predicate: (0, myPredicate.and)(
+        (0, myPredicate.isDivisibleBy)(3),
+        (0, myPredicate.isDivisibleBy)(5)
       ),
       trans: function() {
         return 'FizzBuzz'
       }
     },
     {
-      predicate: (0, predicate_1.or)(
-        (0, predicate_1.isDivisibleBy)(3),
-        (0, predicate_1.contains)(3)
+      predicate: (0, myPredicate.or)(
+        (0, myPredicate.isDivisibleBy)(3),
+        (0, myPredicate.contains)(3)
       ),
       trans: function() {
         return 'Fizz'
       }
     },
     {
-      predicate: (0, predicate_1.or)(
-        (0, predicate_1.isDivisibleBy)(5),
-        (0, predicate_1.contains)(5)
+      predicate: (0, myPredicate.or)(
+        (0, myPredicate.isDivisibleBy)(5),
+        (0, myPredicate.contains)(5)
       ),
       trans: function() {
         return 'Buzz'
       }
     },
     {
-      predicate: predicate_1.otherwise,
+      predicate: myPredicate.otherwise,
       trans: function(n) {
         return n.toString()
       }
     }
   ]
-  return (0, exports.fizzBuzz)(ruleSet)
+  return (0, fizzBuzz)(ruleSet)
 }
 
 export var fizzBuzz = function(ruleSet) {
@@ -56,3 +56,4 @@ export var fizzBuzz = function(ruleSet) {
         })
   }
 }
+
