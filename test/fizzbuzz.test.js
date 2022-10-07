6366a990dad5ca07d251e56f8b73b5f5a6c9f065
MKoding <mikel.romano@540deg.com>
2022-10-07 09:04:35 +0200

    use arrow functions

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,31 +1,31 @@
 const myFizzBuzz = require('../src/fizzbuzz')
 
-describe('FizzBuzz', function() {
-  it('returns "1" for 1 number', function() {
+describe('FizzBuzz', () => {
+  it('returns "1" for 1 number', () => {
     expect(runFizzBuzz(1)).toEqual('1')
   })
-  it('returns "2" for 2 number', function() {
+  it('returns "2" for 2 number', () => {
     expect(runFizzBuzz(2)).toEqual('2')
   })
-  it('returns "Fizz" for 3 number', function() {
+  it('returns "Fizz" for 3 number', () => {
     expect(runFizzBuzz(3)).toEqual('Fizz')
   })
-  it('returns "Fizz" for 6 number', function() {
+  it('returns "Fizz" for 6 number', () => {
     expect(runFizzBuzz(6)).toEqual('Fizz')
   })
-  it('returns "Buzz" for 5 number', function() {
+  it('returns "Buzz" for 5 number', () => {
     expect(runFizzBuzz(5)).toEqual('Buzz')
   })
-  it('returns "Buzz" for 10 number', function() {
+  it('returns "Buzz" for 10 number', () => {
     expect(runFizzBuzz(10)).toEqual('Buzz')
   })
-  it('returns "Buzz" for 5 number', function() {
+  it('returns "Buzz" for 5 number', () => {
     expect(runFizzBuzz(5)).toEqual('Buzz')
   })
-  it('returns "FizzBuzz" for 15 number', function() {
+  it('returns "FizzBuzz" for 15 number', () => {
     expect(runFizzBuzz(15)).toEqual('FizzBuzz')
   })
-  it('returns multiple numbers', function() {
+  it('returns multiple numbers', () => {
     expect(myFizzBuzz.fizzBuzz(53, [1, 2, 3, 4, 5])).toEqual([
       '1',
       '2',
@@ -35,6 +35,4 @@ describe('FizzBuzz', function() {
     ])
   })
 })
-const runFizzBuzz = function(n) {
-  return myFizzBuzz.fizzBuzz(n, [n])[0]
-}
+const runFizzBuzz = number => myFizzBuzz.fizzBuzz(number, [number])[0]
